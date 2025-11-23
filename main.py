21e19b6e90b8e3dc0205d77c3e072076be17a46f
Wilantara <dzikriwildhan@gmail.com>
2025-11-23 16:16:23 +0700

    (Main): delete/cancel balas rating feature

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def main(isUserLoggedIn=False, userLevel="visitor", username="", userId=None):
 
             match user_option:
                 case "1": hotel.searchHotels() # fateeh
-                case "2": pass # omar
+                case "2": pass # fateeh
                 case "3": view.profileView(userId)
                 case "0": main()
                 case _: print("Pilihan tidak valid!")
@@ -42,18 +42,16 @@ def main(isUserLoggedIn=False, userLevel="visitor", username="", userId=None):
             print("===========================")
 
             # option
-            print("1. Daftar Tamu Hotel")
-            print("2. Kelola Pesan & Rating Hotel")
-            print("3. Profile")
+            print("1. Kelola Daftar Tamu Hotel")
+            print("2. Profile")
             print("0. Keluar")
 
             user_option = input("\nMasukan opsi : ")
 
             match user_option:
-                case "1": pass # fateeh
-                case "2": pass # omar
-                case "3": view.profileView(userId)
-                case "0": pass
+                case "1": pass
+                case "2": view.profileView(userId)
+                case "0": main()
                 case _: print("Pilihan tidak valid!")
 
     # User has been not logged in
